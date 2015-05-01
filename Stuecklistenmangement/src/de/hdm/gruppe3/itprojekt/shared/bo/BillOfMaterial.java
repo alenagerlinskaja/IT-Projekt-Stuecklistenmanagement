66a917b5cc2e2a4b8d816cad488af85326e15820
Roxana Dinus <rd018@hdm-stuttgart.de>
2015-05-01 17:12:06 +0200

    Klasse BillOfMaterial angelegt

---
+++ Stuecklistenmangement/src/de/hdm/gruppe3/itprojekt/shared/bo/BillOfMaterial.java
@@ -0,0 +1,40 @@
+package de.hdm.gruppe3.itprojekt.shared.bo;
+
+import java.util.Date;
+
+public class BillOfMaterial extends BusinessObject{
+
+	private String name;
+	private Date creationDate;
+	
+	/**
+	 * Auslesen des Namens der Stückliste
+	 */
+	
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * Setzen des Namens der Stückliste
+	 */
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	/**
+	 * Auslesen des Erstellungsdatums
+	 */
+	public Date getCreationDate() {
+		return creationDate;
+	}
+	
+	/**
+	 * Setzen des Erstellungsdatums
+	 */
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	} 
+	
+}
