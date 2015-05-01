ce8e70fc5cf51474688e8ed9e750182a67778c7c
ALIYE <ag096@hdm-stuttgart.de>
2015-05-01 17:21:01 +0200

    Klasse ComponenetPart angelegt

---
+++ Stuecklistenmangement/src/de/hdm/gruppe3/itprojekt/shared/bo/ComponentPart.java
@@ -0,0 +1,44 @@
+package de.hdm.gruppe3.itprojekt.shared.bo;
+
+public class ComponentPart extends BusinessObject {
+
+	private static final long serialVersionUID = 1L;
+
+		/*
+		 * Textuelle Beschreibung
+		 */
+		private String description = "";
+		
+		/*
+		 * Materialbezeichnung 
+		 */
+		private String material = "";
+		
+		/*
+		 * Auslesen der Textuellen Beschreibung
+		 */
+		public String getDescription(){
+			return this.description;
+		}
+		
+		/*
+		 * Setzen der Textuellen Beschreibung
+		 */
+		public void setDescription(String description){
+			this.description = description;
+		}
+		
+		/*
+		 * Auslesen der Materialbezeichnung
+		 */
+		public String getMaterial(){
+			return this.material;
+		}
+		
+		/*
+		 * Setzen der Materialbezeichnung
+		 */
+		public void setMaterial(String material){
+			this.material = material;
+		}
+}
