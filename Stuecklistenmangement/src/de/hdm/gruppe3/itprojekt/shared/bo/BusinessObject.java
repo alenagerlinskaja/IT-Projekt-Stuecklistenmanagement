cda4ff52d82b24acfc8e1560172fb2bdfa40450d
Alena Gerlinskaja <ag095@hdm-stuttgart.de>
2015-05-01 17:01:00 +0200

    BusinessObject Klasse fertig gestellt

--- Stuecklistenmangement/src/de/hdm/gruppe3/itprojekt/shared/bo/BusinessObject.java
+++ Stuecklistenmangement/src/de/hdm/gruppe3/itprojekt/shared/bo/BusinessObject.java
@@ -1,6 +1,40 @@
 package de.hdm.gruppe3.itprojekt.shared.bo;
 
-public class BusinessObject {
+import java.io.Serializable;
 
-	
+
+public abstract class BusinessObject implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String toString() {
+
+		return this.getClass().getName() + " #" + this.id;
+	}
+
+	public boolean equals(Object object) {
+
+		if (object != null && object instanceof BusinessObject) {
+			BusinessObject businessObject = (BusinessObject) object;
+			try {
+				if (businessObject.getId() == this.id)
+					return true;
+			} catch (IllegalArgumentException e) {
+
+				return false;
+			}
+		}
+
+		return false;
+	}
 }
