86f2fff33a1eb607d0e2ab36c4f86474ba3ef660
Claudia Ryniak <cr073@hdm-stuttgart.de>
2015-05-19 14:52:05 +0200

    Fremdschlüsselbeziehungen eingefügt

--- Stuecklistenmangement/src/de/hdm/gruppe3/itprojekt/shared/bo/Component.java
+++ Stuecklistenmangement/src/de/hdm/gruppe3/itprojekt/shared/bo/Component.java
@@ -2,30 +2,29 @@ package de.hdm.gruppe3.itprojekt.shared.bo;
 
 import java.util.Date;
 
-public class Component extends Element{
-
+public class Component extends ComponentPart {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	/**
 	 * Datum der letzten Änderung eines Users
 	 */
-	
+
 	private Date dateOfModification;
-	
-	/** 
+
+	/**
 	 * Auslesen des Änderungsdatums
 	 */
-	
+
 	public Date getDateOfModification() {
 		return dateOfModification;
 	}
 
-	/** 
+	/**
 	 * Setzen des Änderungsdatums
 	 */
 	public void setDateOfModification(Date dateOfModification) {
 		this.dateOfModification = dateOfModification;
-	} 
-	
+	}
+
 }
