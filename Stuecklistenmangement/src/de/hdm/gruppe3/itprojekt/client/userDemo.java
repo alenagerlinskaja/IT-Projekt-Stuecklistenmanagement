fb8cca311fc7f3dc3b952b5dce817fd3067bde86
ALIYE <ag096@hdm-stuttgart.de>
2015-06-07 10:45:43 +0200

    userDemo Klasse

---
+++ Stuecklistenmangement/src/de/hdm/gruppe3/itprojekt/client/userDemo.java
@@ -0,0 +1,28 @@
+package de.hdm.gruppe3.itprojekt.client;
+
+public class userDemo extends Showcase {
+	
+	/**
+	   * Jeder Showcase besitzt eine einleitende Überschrift, die durch diese
+	   * Methode zu erstellen ist.
+	   * 
+	   * @see Showcase#getHeadlineText()
+	   */
+	  @Override
+	  protected String getHeadlineText() {
+	    return "User";
+	  }
+
+	  /**
+	   * Jeder Showcase muss die <code>run()</code>-Methode implementieren. Sie ist
+	   * eine "Einschubmethode", die von einer Methode der Basisklasse
+	   * <code>ShowCase</code> aufgerufen wird, wenn der Showcase aktiviert wird.
+	   */
+	  @Override
+	  protected void run() {
+	    this.append("Darstellen von User");
+	  }
+	  
+	  
+
+}
